7aefd042c568deb009a1ebc87dca8f4b6fa0a2e2
LD <silvershellcase@gmail.com>
2021-07-08 11:04:36 +0800

    beetlsql分页修改

--- beetlsql-service/src/main/java/com/rust/demo/util/PageUtil.java
+++ beetlsql-service/src/main/java/com/rust/demo/util/PageUtil.java
@@ -9,6 +9,15 @@ import java.util.Map;
 public class PageUtil {
 
     public static PageRequest getPageRequest(Map<String, Object> param) {
-        return (PageRequest) param.getOrDefault(Constant.PAGE_REQUEST, DefaultPageRequest.of(1L, 10));
+        long page;
+        int pageSize;
+        try {
+            page = Long.parseLong((String) param.get(Constant.PAGE));
+            pageSize = Integer.parseInt((String) param.get(Constant.PAGE_SIZE));
+        } catch (Exception ex) {
+            page = 1L;
+            pageSize = 10;
+        }
+        return DefaultPageRequest.of(page, pageSize);
     }
 }
